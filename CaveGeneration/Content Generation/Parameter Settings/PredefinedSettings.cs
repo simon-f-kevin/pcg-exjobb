23fb232cd9a3435c099dccc5c45143badd1f181b
Simon <Simon@SIMON-LAPTOP>
2018-01-22 11:50:23 +0100

    comments

--- CaveGeneration/Content Generation/Parameter Settings/PredefinedSettings.cs
+++ CaveGeneration/Content Generation/Parameter Settings/PredefinedSettings.cs
@@ -12,12 +12,15 @@ namespace CaveGeneration.Content_Generation.Parameter_Settings
         public static Settings settings1 = new Settings()
         {
             EnemyCount = 3,
+            //distance between enemeies
             GoalonGround = true,
             usecopy = true,
             IterationsOfsmoothmap = 2,
+            //number of nighbors for cellular automata
             DistanceBetweenWalks = 5,
             NumberOfSteps = 250,
             NumberOfWalks = 5,
+            //chance to move up, down, left or right
             RandomFillPercent = 45,
             mapGeneratorType = Content_Generation.Map_Generation.MapGeneratorType.DrunkardWalk,
             PlayerLives = 3
